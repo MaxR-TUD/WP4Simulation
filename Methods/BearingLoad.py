16b66e34b15fc05fac8cccd575ab0317348de23f
Maximilien <maximilienroels@gmail.com>
2024-11-21 17:20:19 +0100

    End session 2

--- Methods/BearingLoad.py
+++
@@ -1,6 +0,0 @@
-import VariableSetup
-
-
-def bearing_check(hole, t2, t3):
-
-    return
--- Methods/PullPushThrough.py
+++ Methods/PullPushThrough.py
@@ -6,3 +6,8 @@ def pull_push_check(hole, r_i=0, t2=0, t3=0):  # hole is a hole retard
     stress_t2 = 2 * hole.po / (math.pi * r_i * t2)  # [Pa]
     stress_t3 = 2 * hole.po / (math.pi * r_i * t3)  # [Pa]
     return [stress_t2, stress_t3]
+
+
+def bearing_check(hole, ):
+
+    return
